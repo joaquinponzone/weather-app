ba4111a88c287660c10727745734f65409428f47
joaquinponzone <ponzonejoaquin@gmail.com>
2021-05-22 19:26:08 -0300

    wdr v2.2

--- client/src/components/MainNavigation.jsx
+++ client/src/components/MainNavigation.jsx
@@ -5,8 +5,8 @@ import './MainNavigation.css';
 export default function MainNavigation({themeToggler, theme}) {
    return(
       <div className="main-nav">
-         <NavLink to='/about' activeClassName="link-act">Acerca de</NavLink>
-         <NavLink to='/credits' activeClassName="link-act">Créditos</NavLink>
+         <NavLink to='/about' className="switch" >Acerca de</NavLink>
+         <NavLink to='/credits' className="switch" >Créditos</NavLink>
          <NavLink to='/' class="switch" onClick={themeToggler} className='form'>{theme === 'light' ? 'to Dark Mode' : 'to Light Mode'}</NavLink>
       </div>
    )
